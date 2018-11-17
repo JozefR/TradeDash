c9fe4c58eed04303e833db8679861b8ca77ec161
Jozef Randjak <randjakjozef@gmail.com>
2018-11-17 14:55:09 +0100

    bugfix: specify precision for decimal values

--- TradeDash.BackEnd/Models/ApplicationDbContext.cs
+++ TradeDash.BackEnd/Models/ApplicationDbContext.cs
@@ -12,5 +12,20 @@ namespace TradeDash.BackEnd.Models
 
         public DbSet<MoneyManagement> MoneyManagements { get; set; }
         public DbSet<Strategy> Strategies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MoneyManagement>()
+                .Property(x => x.AccountValue)
+                .HasColumnType("decimal(9,6)");
+            
+            modelBuilder.Entity<MoneyManagement>()
+                .Property(x => x.ToBuyAll)
+                .HasColumnType("decimal(9,6)");
+            
+            modelBuilder.Entity<MoneyManagement>()
+                .Property(x => x.AmountAvIfAllBought)
+                .HasColumnType("decimal(9,6)");
+        }
     }
 }
