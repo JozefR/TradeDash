cf66c3f681de7e718dd906364598bf113b05fe3a
Jozef Randjak <randjakjozef@gmail.com>
2018-11-22 21:37:40 +0100

    add name to strategy table

---
+++ TradeDash.BackEnd/Migrations/20181122203430_AddNamePropertyToStrategy.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TradeDash.BackEnd.Migrations
+{
+    public partial class AddNamePropertyToStrategy : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EstaminateReturns_Strategies_StrategyId",
+                table: "EstaminateReturns");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Strategies",
+                maxLength: 36,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StrategyId",
+                table: "EstaminateReturns",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EstaminateReturns_Strategies_StrategyId",
+                table: "EstaminateReturns",
+                column: "StrategyId",
+                principalTable: "Strategies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EstaminateReturns_Strategies_StrategyId",
+                table: "EstaminateReturns");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Strategies");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StrategyId",
+                table: "EstaminateReturns",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EstaminateReturns_Strategies_StrategyId",
+                table: "EstaminateReturns",
+                column: "StrategyId",
+                principalTable: "Strategies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- TradeDash.DTO/Strategy.cs
+++ TradeDash.DTO/Strategy.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TradeDash.DTO
 {
     public class Strategy
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(36)]
+        public string Name { get; set; }
 
         public StrategyType StrategyType { get; set; }
     }    
