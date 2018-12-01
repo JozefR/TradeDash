c88382f293c8c8d1503af1708bea1cc8abb59731
Jozef Randjak <randjakjozef@gmail.com>
2018-12-01 16:17:34 +0100

    Add new Application project, add generic enum to select method

---
+++ TradeDash.Application/Common/ViewHelpers.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace TradeDash.Application.Common
+{
+    public static class ViewHelpers
+    {
+        public static List<SelectListItem> EnumToSelectItem<T>() where T : System.Enum
+        {
+            var results = new List<SelectListItem>();
+            var values = Enum.GetValues(typeof(T));
+
+            foreach (int value in values)
+            {
+                var selectListItem = new SelectListItem
+                {
+                    Text = Enum.GetName(typeof(T), value),
+                    Value = value.ToString()
+                };
+                
+                results.Add(selectListItem);
+            }
+
+            return results;
+        }
+    }
+}
