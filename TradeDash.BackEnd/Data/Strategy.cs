03b13181b226920b93a9a3d89ae54e281700e8a6
Jozef Randjak <randjakjozef@gmail.com>
2019-02-02 18:16:27 +0100

    refactor remove stock and option trades from strategy

--- TradeDash.BackEnd/Data/Strategy.cs
+++ TradeDash.BackEnd/Data/Strategy.cs
@@ -9,9 +9,5 @@ namespace TradeDash.BackEnd.Data
         public ICollection<EstaminateReturn> EstaminateReturns { get; set; }
         
         public ICollection<ReturnOnStrategy> ReturnOnStrategy { get; set; }
-
-        public ICollection<StockTrade> StockTrades { get; set; }
-
-        public ICollection<OptionTrade> OptionTrades { get; set; }
     }
 }
---
+++ TradeDash.BackEnd/Migrations/20190202171447_RemoveStockAndOptionTradesFromStrategy.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TradeDash.BackEnd.Migrations
+{
+    public partial class RemoveStockAndOptionTradesFromStrategy : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OptionTrades_Strategies_StrategyId",
+                table: "OptionTrades");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_StockTrades_Strategies_StrategyId",
+                table: "StockTrades");
+
+            migrationBuilder.DropIndex(
+                name: "IX_StockTrades_StrategyId",
+                table: "StockTrades");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OptionTrades_StrategyId",
+                table: "OptionTrades");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_StockTrades_StrategyId",
+                table: "StockTrades",
+                column: "StrategyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OptionTrades_StrategyId",
+                table: "OptionTrades",
+                column: "StrategyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OptionTrades_Strategies_StrategyId",
+                table: "OptionTrades",
+                column: "StrategyId",
+                principalTable: "Strategies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StockTrades_Strategies_StrategyId",
+                table: "StockTrades",
+                column: "StrategyId",
+                principalTable: "Strategies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
